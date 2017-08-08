9b7853220118f32632de504f6ab5c044efc67945
sarvenaz khaksarfard <s.khaksarfard@gmail.com>
2017-08-08 00:04:09 -0400

    Offer updated

--- src/app/Offer/Offer.component.ts
+++ src/app/Offer/Offer.component.ts
@@ -7,7 +7,53 @@ import {IOffer} from "./Offer.model";
 @Component({
   templateUrl: './Offer.component.html',
   styles:[`
-
+			body{
+		margin: 0;
+		padding: 0;
+		background-color: rgb(0,200,200);
+		font-size: 17px;
+	}
+	form{
+	
+		width:80%;
+		text-align: center;
+		margin: 0 auto;
+		margin-top: 10%;
+		
+		}
+		div:first-child{
+		height: 80px;
+		width: 100%;
+			position: relative;
+			margin-bottom: 50px;
+			
+		}
+		label{
+			position: absolute;
+			top: 10px;
+			left: 100px;
+			width: 130px;
+			}
+		#offerId{
+			position: absolute;
+			top: 10px;
+			left: 250px;
+			width: 180px;
+			
+		}
+		div:first-child button{
+			position: absolute;
+			top: 10px;
+			left: 450px;
+		}
+		
+	button{
+		background-color: rgb(0,128,128);
+		color:white;
+		width: 120px;
+		height: 40px;
+		
+		}
   `
   ]
 })
